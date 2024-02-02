eb4a0c8f4b70ad943fe2defd1faf8bbe51c2ab85
JJCWG <125705166+JJCWG@users.noreply.github.com>
2024-02-01 19:40:44 -0500

    Pushing Completed Lab 2 to new branch of Labs repo

---
+++ app/src/main/java/com/example/lab2_codepathmail/Email.kt
@@ -0,0 +1,9 @@
+package com.example.lab2_codepathmail
+
+class Email(
+    val sender: String,
+    val title: String,
+    val summary: String,
+    val senderImage: Int,
+    val date: String) {
+}
